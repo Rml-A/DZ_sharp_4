0a02a0cbfa3af1a43b3aa99a90b1aa6bf9d14446
Ram <rmailtat777@gmail.com>
2023-02-20 19:59:25 +0300

    git tupit)

---
+++ task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; num != 0; num /= 10)
+{
+    i += num % 10;
+    if (num < 10)
+    {
+        System.Console.WriteLine(i);
+    }
+
+}
+
