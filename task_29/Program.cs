e762342e140d419e6bbdb7efdf14fd9763bd6684
Ram <rmailtat777@gmail.com>
2023-02-20 21:04:18 +0300

    Вроде норм

---
+++ task_29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] GetArr(int size, int leftRange, int rightRange)
+{
+
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(leftRange, rightRange);
+    return arr;
+}
+
+
+void PrintArray(int[] mass)
+{
+
+    System.Console.Write("[" + string.Join(", ", mass) +"]");
+
+}
+
+int[] array = GetArr(8, 0, 101);
+PrintArray(array);
